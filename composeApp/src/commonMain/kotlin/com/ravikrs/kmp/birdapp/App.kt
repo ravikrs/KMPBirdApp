a0acf3b31d37c8d8d582a62e2a9f0f57f19e3b5a
Ravi Singh <17711778+ravikrs@users.noreply.github.com>
2024-09-15 12:30:53 +0200

    show images along with category

--- composeApp/src/commonMain/kotlin/com/ravikrs/kmp/birdapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/ravikrs/kmp/birdapp/App.kt
@@ -1,37 +1,85 @@
 package com.ravikrs.kmp.birdapp
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpbirdapp.composeapp.generated.resources.Res
-import kmpbirdapp.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdsViewModel() })
+        val uiState by birdsViewModel.uiState.collectAsState()
+        LaunchedEffect(birdsViewModel) {
+            birdsViewModel.updateImages()
+        }
+
+//        Text(uiState.images.toString(), Modifier.verticalScroll(rememberScrollState()))
+        BirdsPage(uiState, onSelectCategory = { birdsViewModel.selectCategory(it) })
+    }
+}
+
+@Composable
+fun BirdsPage(uiState: BirdsUiState, onSelectCategory: (String) -> Unit) {
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
+        ) {
+            for (category in uiState.categories) {
+                Button(
+                    onClick = { onSelectCategory(category) },
+                    modifier = Modifier.aspectRatio(1.0f).weight(1.0f)
+                ) {
+                    Text(category)
+                }
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        }
+        AnimatedVisibility(visible = uiState.selectedImages.isNotEmpty()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(180.dp),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp)
+            ) {
+                items(uiState.selectedImages) { image ->
+                    BirdImageCell(image)
                 }
             }
         }
     }
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        resource = asyncPainterResource("https://sebi.io/demo-image-api/${image.path}"),
+        contentDescription = "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth().aspectRatio(1.0f)
+    )
+
 }
