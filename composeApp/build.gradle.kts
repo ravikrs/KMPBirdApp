09a199d4350fec12a9c873fdc76b44ddd7593c84
Ravi Singh <17711778+ravikrs@users.noreply.github.com>
2024-09-15 10:51:43 +0200

    add deps for ktor,moko-mvvm, kamel

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinxSerialization)
+
 }
 
 kotlin {
